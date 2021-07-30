a3297ea6643d46aad13d04832228951e780ea5e4
Ali Alladin <ali.alladin20@gmail.com>
2021-07-30 16:18:19 +0200

    Created my first component.

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,11 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
   const name = 'Ali';
   const x = true
 
   return (
     <div className="App">
-      <h1>Hello from React</h1>
-      <h2>Hello {name}</h2>
+      <Header></Header>
     </div>
     
   );
